59576dc664d3c35d5229838bc1c6e4775ce53c13
aswathi_sivasubramanian <skaswathisk@gmail.com>
2024-01-05 18:21:22 +0530

    docker files added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -15,16 +15,13 @@ interface LinkProps {
     { title: "Home", href: "#" },
     { title: "About", href: "#" },
     { title: "Services", href: "#" },
-    { title: "Contact", href: "#" },
-    { title: "Home", href: "#" },
-    { title: "About", href: "#" },
-    { title: "Services", href: "#" },
-    { title: "Contact", href: "#" },
+    
+    
     
 ];
   
 const Sidebar:React.FC=()=>{
-    return(<div ><div className=" mt-0.5 "><nav className="bg-customBlue flex-col w-36 h-full pt-36 overflow-y-auto">
+    return(<div ><div className=" mt-0.5 "><nav className="bg-customBlue flex-col w-36 pt-36 overflow-y-auto h-screen">
     {navigationLinks.map((link) => (
       <div key={link.title} className="mt-2 ml-4 ">
         <a href={link.href} className="px-3 py-2 text-white hover:text-customOrange transition-all duration-150 ease-in-out">
